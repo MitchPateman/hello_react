263830f8af12a2260539260a68bed0f26896cf04
Mitch Pateman <mitch_pateman@hotmail.com>
2016-08-14 02:32:21 -0400

    Added a Like button

--- dev/index.jsx
+++ dev/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import LikesComponent from './LikesComponent.jsx';
+
 
 var HelloWorld = React.createClass({
   render: function() {
     return (
       <p>Hello, {this.props.greetTarget}!</p>
-      
     );
   }
 });
@@ -19,6 +20,8 @@ ReactDOM.render(
     <HelloWorld greetTarget="Bono"/>
     <HelloWorld greetTarget="Catwoman"/>
     <HelloWorld greetTarget="Catherine Zeta Jones"/>
+    <HelloWorld greetTarget="Steve Jobs"/>
+    <LikesComponent />
   </div>,
   document.querySelector("#container")
 );
